978167f089dd5a047938823dcfdd6485403cda1b
DavidDaniel18 <david.vermette-nadeau.1@ens.etsmtl.ca>
2025-02-16 18:41:28 -0500

    Commented out https config

--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -8,14 +8,14 @@ public class Program
             {
                 webBuilder.UseStartup<Startup>();
 
-                webBuilder.ConfigureKestrel(serverOptions =>
-                {
-                    serverOptions.ListenLocalhost(8080);
-                    serverOptions.ListenLocalhost(8081, listenOptions =>
-                    {
-                        listenOptions.UseHttps("certs/localhost.pfx", "");
-                    });
-                });
+                //webBuilder.ConfigureKestrel(serverOptions =>
+                //{
+                //    serverOptions.ListenLocalhost(8080);
+                //    serverOptions.ListenLocalhost(8081, listenOptions =>
+                //    {
+                //        listenOptions.UseHttps("certs/localhost.pfx", "");
+                //    });
+                //});
             });
 
     public static void Main(string[] args)
